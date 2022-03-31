4b7993fdd9a664d034d7bc6cd78035a83e7a6204
mdomina <mike.sondonline@gmail.com>
2022-03-31 15:06:52 +0200

    update main

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ Acesso a Shoda:
 
 Impostare i targets:
 [+] aggiungere i targets nel file config.ini e usare l'opzione --target-file
-[+] usare l'opzione --target <IP>
+[+] usare l'opzione --target <IP> --api <APIKEY>
 
 [//] ATTENZIONE un metodo esclude l'altro [//]
 """
 parser = argparse.ArgumentParser(description=description, formatter_class=RawTextHelpFormatter)
 parser.add_argument('--target', help='Single target IP', type=str)
+parser.add_argument('--api', help='APIkey Shodan', type=str)
 parser.add_argument('--file', help='Legge il target dal file confing.ini', type=str)
 
 # istanzio gli argomenti
 args = parser.parse_args()
 
-# file di configurazione
-config = configparser.ConfigParser()
-config.read('config.ini')
-apikey = config['shodan']['apikey']
-target_list = list()
-
 if args.target:
-    api_shodan(apikey, args.target)
+    api_shodan(args.api, args.target)
 elif args.file:
+    # file di configurazione
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    apikey = config['shodan']['apikey']
+    target_list = list()
     for section in config.sections():# ciclo tutte le sezioni
         if section == 'shodan':
             continue
